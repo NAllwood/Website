7a8773981476e0d61f3380bac9babd85574974b5
NAllwood <nevinallwood@gmail.com>
2018-04-30 22:54:27 +0200

    better spacing and navbar snap

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,14 @@
 
 function update_navbar() {
   var navbar = document.getElementById("navbar");
+
   var articles = document.getElementById("articles");
-  var about = document.getElementById("about");
-  var projects = document.getElementById("projects");
-  var fullscreen_image = document.getElementById("fullscreen_image");
+  var about = document.getElementById("about_container");
+  var projects = document.getElementById("projects_container");
+  var contact = document.getElementById("contact_container");
+
+  var about_content = document.getElementById("about_article");
+
 
   var nav_about = document.getElementById("nav_about");
   var nav_projects = document.getElementById("nav_projects");
@@ -12,9 +16,8 @@ function update_navbar() {
 
   var about_title = document.getElementById("about_title");
 
-  var nav_height = navbar.getBoundingClientRect().height
+  var nav_height = navbar.getBoundingClientRect().height;
 
-  console.log("window",window.pageYOffset ,"active",)
 
   /* fix or unfix navbar */
   if (window.pageYOffset >= articles.offsetTop - nav_height) {
@@ -27,35 +30,40 @@ function update_navbar() {
   }
 
   /* activate right articles in navbar */
-  if (window.pageYOffset >= about.offsetTop -120-nav_height && window.pageYOffset < projects.offsetTop -120-nav_height){
+  if (window.pageYOffset >= about.offsetTop -64 -nav_height && window.pageYOffset < projects.offsetTop -64 -nav_height){
     nav_about.classList.add("active");
+    about_content.classList.add("fade_in");
     nav_projects.classList.remove("active");
     nav_contact.classList.remove("active");
-  }else if (window.pageYOffset >= projects.offsetTop -120-nav_height && window.pageYOffset < contact.offsetTop -120-nav_height){
+  }else if (window.pageYOffset >= projects.offsetTop -64 -nav_height && window.pageYOffset < contact.offsetTop -64 -nav_height){
     nav_about.classList.remove("active");
     nav_projects.classList.add("active");
     nav_contact.classList.remove("active");
-  }else if (window.pageYOffset >= contact.offsetTop -120-nav_height && window.pageYOffset < contact.offsetBottom -120-nav_height){
+  }else if (window.pageYOffset >= contact.offsetTop -64 -nav_height && window.pageYOffset < contact.offsetBottom -64 -nav_height){
     nav_about.classList.remove("active");
     nav_projects.classList.remove("active");
     nav_contact.classList.add("active");
   }
 
-  if (window.pageYOffset >= about.offsetTop -120-nav_height){
+  if (window.pageYOffset >= about.offsetTop -64-nav_height){
     about_title.classList.add("slide_in_left");
   }
-  if (window.pageYOffset >= projects.offsetTop -120-nav_height){
+  if (window.pageYOffset >= projects.offsetTop -64-nav_height){
     projects_title.classList.add("slide_in_left");
   }
-  if (window.pageYOffset >= contact.offsetTop -120-nav_height){
+  if (window.pageYOffset >= contact.offsetTop -64-nav_height){
     contact_title.classList.add("slide_in_left");
   }
 
 
 }
 
-function scroll_to(element, duration) {
-  elementY = element.offsetTop;
+function scroll_to(element, duration, with_navbar) {
+  var elementY = element.offsetTop;
+  if (with_navbar == 1){
+    var navbar_height = document.getElementById("navbar").getBoundingClientRect().height;
+    var elementY = element.offsetTop-navbar_height;
+  }
   var startingY = window.pageYOffset;
   var diff = elementY - startingY;
   console.log("elementY",elementY);
